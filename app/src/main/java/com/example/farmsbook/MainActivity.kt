c29e6089d8b057841a7acf46ba3c8325b69e7843
Sachit Goel <goelsachit2002@gmail.cpm>
2023-07-03 15:39:28 +0530

    Initial Commit

--- app/src/main/java/com/example/farmsbook/MainActivity.kt
+++ app/src/main/java/com/example/farmsbook/MainActivity.kt
@@ -1,29 +1,43 @@
 package com.example.farmsbook
 
-import android.content.Context
+import android.app.ProgressDialog
 import android.content.Intent
-import android.database.Cursor
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.farmsbook.databinding.ActivityMainBinding
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
 
     var request_code = 1000
+    var imageuri:Uri?=null
 
-    var binding: ActivityMainBinding? = null
+    var storage:FirebaseStorage?=null
+    var storage_ref:StorageReference?=null
+
+    var binding:ActivityMainBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        binding?.photoBtn?.setOnClickListener() {
+        storage = FirebaseStorage.getInstance()
+        storage_ref = storage?.getReference()
 
-            var intent = Intent(Intent.ACTION_PICK)
+        binding?.photoBtn?.setOnClickListener(){
+
+            var intent=Intent(Intent.ACTION_PICK)
             intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(intent, request_code)
+        }
+
+        binding?.uploadBtn?.setOnClickListener(){
+            uploadImage()
         }
     }
 
@@ -34,10 +48,29 @@ class MainActivity : AppCompatActivity() {
 
             if (requestCode == request_code) {
 
-                binding?.ivPhoto?.setImageURI(data?.data)
+                imageuri=data?.data
+                binding?.ivPhoto?.setImageURI(imageuri)
 
             }
         }
+    }
+
+    fun uploadImage() {
+
+
+            val progressdialog = ProgressDialog(this)
+            progressdialog.setMessage("Uploading the image")
+            progressdialog.show()
+
+        storage_ref = storage_ref?.child("images/" + UUID.randomUUID().toString())
+
+        storage_ref?.putFile(imageuri!!)?.addOnSuccessListener {
+            progressdialog.dismiss()
+            Toast.makeText(applicationContext, "Image Uploaded Successfully", Toast.LENGTH_SHORT).show()
+        }
+            ?.addOnFailureListener{
+                Toast.makeText(applicationContext, "Failed to upload the image", Toast.LENGTH_SHORT).show()
+            }
 
 
     }
